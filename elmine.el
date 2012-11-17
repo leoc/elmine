091f61c70c9e7630a74b7b127488051d143a35e7
Arthur Andersen <leoc.git@gmail.com>
2012-11-17 16:20:10 +0100

    Add elisp header with license info

--- elmine.el
+++ elmine.el
@@ -1,3 +1,28 @@
+;;; elmine.el -- redmine API access for elisp
+
+;; Copyright (C) 2012 Arthur Leonard Andersen
+
+;; Author: Arthur Andersen <leoc DOT git AT gmail.com>
+;; Version: 0.3
+;; Keywords: tools
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 (require 'json)
 
 (defun plist-merge (base new)
