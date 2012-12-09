e1ed2d4f551f866515ac606fe9d46ae2859c0cd2
Arthur Andersen <leoc.git@gmail.com>
2012-12-09 22:39:17 +0100

    Update file header

--- elmine.el
+++ elmine.el
@@ -1,27 +1,37 @@
-;;; elmine.el -- redmine API access for elisp
+;;; elmine.el --- Redmine API access via elisp.
 
-;; Copyright (C) 2012 Arthur Leonard Andersen
-
+;; Copyright (c) 2012 Arthur Leonard Andersen
+;;
 ;; Author: Arthur Andersen <leoc.git@gmail.com>
+;; URL: http://github.com/leoc/elmine
 ;; Version: 0.3
 ;; Keywords: tools
-
-;; This file is NOT part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; `elmine` provides simple means of accessing the redmine Rest API
+;; programmatically. This means that you do not have interactive
+;; functions but functions that give and take list representations of
+;; JSON objects of the redmine API.
+
+;;; Code:
 
 (require 'json)
 
